07707349d07ea484cad2cf8e3b27343ba087563d
Sutanisurafu <sutanisurafu@yandex.ru>
2022-12-14 13:03:34 +0500

    добавление карточек через js

--- scripts/index.js
+++ scripts/index.js
@@ -9,18 +9,17 @@ const popupForm = document.querySelector('.popup__form');
 const inputName = document.querySelector('.popup__input_type_name');
 const inputSpeciality = document.querySelector('.popup__input_type_speciality');
 
+
 //открытие попапа
 function popupOpen() {
   popup.classList.add('popup_opened');
   inputName.value = profileName.textContent;
   inputSpeciality.value = profileSpeciality.textContent;
 }
-
 //закрытие попапа
 function popupClose() {
   popup.classList.remove('popup_opened');
 }
-
 //сохранение данных формы 
 function handleFormSubmit (evt) {
   evt.preventDefault();
@@ -29,26 +28,84 @@ function handleFormSubmit (evt) {
   popupClose();
 }
 
-
 popupCloseButton.addEventListener('click', popupClose);
 profileEditButton.addEventListener('click', popupOpen);
 popupForm.addEventListener('submit', handleFormSubmit);
 
 
 
-
-
-
-//кнопка лайк
-for (let i = 0; i < cardLikeButton.length; i++) {
-  cardLikeButton[i].addEventListener("click", function liki() {
-    if(cardLikeButton[i].classList.contains('like-btn_active')) {
-      cardLikeButton[i].classList.remove("like-btn_active")
-    } else {cardLikeButton[i].classList.add("like-btn_active");
+const cardsArr = [
+  {
+    place: 'Чусовая',
+    link: 'https://www.visit-tagil.ru/upload/iblock/fcb/chusovaya_reka_.jpg'
+  },
+  {
+    place: 'Кунгурская пещера',
+    link: 'https://lh3.googleusercontent.com/p/AF1QipNRgZutp5r7YNZZkrP-Xsi0PobkaXARlbIfaYtj=s680-w680-h510'
+  },
+  {
+    place: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    place: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    place: 'Санкт-Петербург',
+    link: 'https://images.unsplash.com/photo-1548834925-e48f8a27ae6f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+  },
+  {
+    place: 'Байкал',
+    link: 'https://images.unsplash.com/photo-1617835594990-7cd5a9b5d153?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
   }
-    
-  });
+]; 
+//пробую добавить карточки через JS 3
+
+const cardsContainer = document.querySelector('.cards');
+
+const createCard = (place, link) => {
+  const card = document.createElement('article');
+  card.classList.add('card');
+  const cardImage = document.createElement('img');
+  cardImage.classList.add('card__image')
+  const cardTitle = document.createElement('h2');
+  const cardLikeBtn = document.createElement('button');
+  cardImage.textContent = link
+  cardTitle.textContent = place
+  cardTitle.classList.add('card__title');
+  cardLikeBtn.classList.add('like-btn');
+  card.append(cardImage, cardTitle, cardLikeBtn);
+  return card;
 }
+
+
+const cardsTemplate = document.querySelector('#cards-template').content;
+cardsArr.forEach(function (element) {
+  const cardsElement = cardsTemplate.cloneNode(true);
+
+  cardsElement.querySelector('.card__title').textContent = element.place;
+  cardsElement.querySelector('.card__image').src = element.link;
+  cardsContainer.append(cardsElement);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -62,3 +119,16 @@ for (let i = 0; i < cardLikeButton.length; i++) {
 // popupContainer.addEventListener('click', function(event) {
 //   event.preventDefault();
 // })
+
+//кнопка лайк
+// for (let i = 0; i < cardLikeButton.length; i++) {
+//   cardLikeButton[i].addEventListener("click", function liki() {
+//     if(cardLikeButton[i].classList.contains('like-btn_active')) {
+//       cardLikeButton[i].classList.remove("like-btn_active")
+//     } else {cardLikeButton[i].classList.add("like-btn_active");
+//   }
+    
+//   });
+// }
+
+
